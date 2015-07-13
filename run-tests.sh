1beaf9466703684235d4c7f7e1abb0ec28c2b54c
Andreas Lutro <anlutro@gmail.com>
2015-07-13 13:44:59 +0200

    fix test runner

--- run-tests.sh
+++ run-tests.sh
@@ -7,14 +7,15 @@ else
 fi
 if [ "$php_version" != "5.3" ] && [ "$php_version" != "hhvm" ]; then
 	php -S localhost:8080 -t tests/server > /dev/null 2>&1 &
+	php_pid=$!
 	export CURL_TEST_SERVER_RUNNING=1
 fi
 
 phpunit $phpunit_args
 ret=$?
 
-if [ $server_support ]; then
-	pkill php
+if [ $CURL_TEST_SERVER_RUNNING ]; then
+	kill $php_pid
 fi
 
 exit $ret
