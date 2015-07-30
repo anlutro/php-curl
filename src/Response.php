670765cb24f7059a668ff0f8b80ada6730ce1195
serg <ku1yamin_sergey@mail.ru>
2015-07-30 12:22:53 +0300

    -- psr-2

--- src/Response.php
+++ src/Response.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * PHP OOP cURL
- * 
+ *
  * @author   Andreas Lutro <anlutro@gmail.com>
  * @license  http://opensource.org/licenses/MIT
  * @package  PHP cURL
@@ -14,96 +14,95 @@ namespace anlutro\cURL;
  */
 class Response
 {
-	/**
-	 * The response headers.
-	 *
-	 * @var array
-	 */
-	public $headers = array();
+    /**
+     * The response headers.
+     *
+     * @var array
+     */
+    public $headers = array();
 
-	/**
-	 * The response body.
-	 *
-	 * @var string
-	 */
-	public $body;
+    /**
+     * The response body.
+     *
+     * @var string
+     */
+    public $body;
 
-	/**
-	 * The results of curl_getinfo on the response request.
-	 *
-	 * @var array|false
-	 */
-	public $info;
+    /**
+     * The results of curl_getinfo on the response request.
+     *
+     * @var array|false
+     */
+    public $info;
 
-	/**
-	 * The response code including text, e.g. '200 OK'.
-	 *
-	 * @var string
-	 */
-	public $statusText;
+    /**
+     * The response code including text, e.g. '200 OK'.
+     *
+     * @var string
+     */
+    public $statusText;
 
-	/**
-	 * The response code.
-	 *
-	 * @var int
-	 */
-	public $statusCode;
+    /**
+     * The response code.
+     *
+     * @var int
+     */
+    public $statusCode;
 
-	/**
-	 * @param string $body
-	 * @param array  $headers
-	 * @param mixed  $info
-	 */
-	public function __construct($body, $headers, $info = array())
-	{
-		$this->body = $body;
-		$this->info = $info;
-		if (is_array($headers)) {
-			$this->headers = $headers;
-		} else {
-			$this->headers = static::headerToArray($headers);
-		}
+    /**
+     * @param string $body
+     * @param array $headers
+     * @param mixed $info
+     */
+    public function __construct($body, $headers, $info = array())
+    {
+        $this->body = $body;
+        $this->info = $info;
+        if (is_array($headers)) {
+            $this->headers = $headers;
+        } else {
+            $this->headers = static::headerToArray($headers);
+        }
 
-		if (isset($this->headers['http/1.1'])) {
-			$this->setCode($this->headers['http/1.1']);
-		} elseif (isset($this->headers['http/1.0'])) {
-			$this->setCode($this->headers['http/1.0']);
-		}
-	}
+        if (isset($this->headers['http/1.1'])) {
+            $this->setCode($this->headers['http/1.1']);
+        } elseif (isset($this->headers['http/1.0'])) {
+            $this->setCode($this->headers['http/1.0']);
+        }
+    }
 
-	/**
-	 * Turn a header string into an array.
-	 *
-	 * @param  string $header
-	 *
-	 * @return array
-	 */
-	protected static function headerToArray($header)
-	{
-		$headerLines = explode("\r\n", $header);
-		$headers = array();
+    /**
+     * Turn a header string into an array.
+     *
+     * @param  string $header
+     *
+     * @return array
+     */
+    protected static function headerToArray($header)
+    {
+        $headerLines = explode("\r\n", $header);
+        $headers = array();
 
-		foreach ($headerLines as $header) {
-			$key = null;
-			$val = null;
-			$delimiter = strpos($header, ': ');
+        foreach ($headerLines as $header) {
+            $key = null;
+            $val = null;
+            $delimiter = strpos($header, ': ');
 
-			if ($delimiter !== false) {
-				$key = substr($header, 0, $delimiter);
-				$val = substr($header, $delimiter + 2);
-			} else {
-				$delimiter = strpos($header, ' ');
-				if ($delimiter !== false) {
-					$key = substr($header, 0, $delimiter);
-					$val = substr($header, $delimiter + 1);
-				}
-			}
+            if ($delimiter !== false) {
+                $key = substr($header, 0, $delimiter);
+                $val = substr($header, $delimiter + 2);
+            } else {
+                $delimiter = strpos($header, ' ');
+                if ($delimiter !== false) {
+                    $key = substr($header, 0, $delimiter);
+                    $val = substr($header, $delimiter + 1);
+                }
+            }
 
             if ($key !== null) {
                 $key = strtolower($key);
                 if (isset($headers[$key]) && strpos($header, ': ') !== false) {
-                    if (!is_array($headers[$key]))
-                    {
+                    if (!is_array($headers[$key])) {
                         $headers[$key] = [$headers[$key]];
                     }
                     $headers[$key][] = $val;
@@ -111,79 +110,79 @@ class Response
                     $headers[$key] = $val;
                 }
             }
-		}
+        }
 
-		return $headers;
-	}
+        return $headers;
+    }
 
-	/**
-	 * Set the response code.
-	 *
-	 * @param string $code
-	 */
-	protected function setCode($code)
-	{
-		$this->statusText = $code;
-		$code = explode(' ', $code);
-		$this->statusCode = (int) $code[0];
-	}
+    /**
+     * Set the response code.
+     *
+     * @param string $code
+     */
+    protected function setCode($code)
+    {
+        $this->statusText = $code;
+        $code = explode(' ', $code);
+        $this->statusCode = (int)$code[0];
+    }
 
-	/**
-	 * Get a specific header from the response.
-	 *
-	 * @param  string $key
-	 *
-	 * @return mixed
-	 */
-	public function getHeader($key)
-	{
-		$key = strtolower($key);
+    /**
+     * Get a specific header from the response.
+     *
+     * @param  string $key
+     *
+     * @return mixed
+     */
+    public function getHeader($key)
+    {
+        $key = strtolower($key);
 
-		return array_key_exists($key, $this->headers) ?
-			$this->headers[$key] : null;
-	}
+        return array_key_exists($key, $this->headers) ?
+            $this->headers[$key] : null;
+    }
 
-	/**
-	 * Gets all the headers of the response.
-	 *
-	 * @return array
-	 */
-	public function getHeaders()
-	{
-		return $this->headers;
-	}
+    /**
+     * Gets all the headers of the response.
+     *
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
 
-	/**
-	 * Convert the response instance to an array.
-	 *
-	 * @return array
-	 */
-	public function toArray()
-	{
-		return array(
-			'headers' => $this->headers,
-			'body' => $this->body,
-			'info' => $this->info
-		);
-	}
+    /**
+     * Convert the response instance to an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return array(
+            'headers' => $this->headers,
+            'body' => $this->body,
+            'info' => $this->info
+        );
+    }
 
-	/**
-	 * Convert the response object to a JSON string.
-	 *
-	 * @return string
-	 */
-	public function toJson()
-	{
-		return json_encode($this->toArray());
-	}
+    /**
+     * Convert the response object to a JSON string.
+     *
+     * @return string
+     */
+    public function toJson()
+    {
+        return json_encode($this->toArray());
+    }
 
-	/**
-	 * Convert the object to its string representation by returning the body.
-	 *
-	 * @return string
-	 */
-	public function __toString()
-	{
-		return $this->body;
-	}
+    /**
+     * Convert the object to its string representation by returning the body.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->body;
+    }
 }
