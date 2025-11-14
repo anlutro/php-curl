26584edb22cdb95c2a52358e4caed0fe1cec6022
Andreas Lutro <anlutro@gmail.com>
2025-11-14 12:13:21 +0100

    fix cURLTest for new phpunit

--- tests/functional/cURLTest.php
+++ tests/functional/cURLTest.php
@@ -1,13 +1,15 @@
 <?php
 
-/**
- * @group server
- */
-class cURLTest extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Test;
+
+#[Group("server")]
+class cURLTest extends TestCase
 {
 	const URL = 'http://localhost:8080';
 
-	public function setUp()
+	public function setUp(): void
 	{
 		if (!getenv('CURL_TEST_SERVER_RUNNING')) {
 			$this->markTestSkipped('The web server is not running.');
@@ -22,7 +24,7 @@ class cURLTest extends PHPUnit_Framework_TestCase
 		return new anlutro\cURL\cURL;
 	}
 
-	/** @test */
+	#[Test]
 	public function successfulResponse()
 	{
 		$r = $this->makeCurl()->get(static::URL.'/success.php');
@@ -33,7 +35,7 @@ class cURLTest extends PHPUnit_Framework_TestCase
 		$this->assertNotNull($r->info);
 	}
 
-	/** @test */
+	#[Test]
 	public function failedResponse()
 	{
 		$r = $this->makeCurl()->get(static::URL.'/failure.php');
@@ -44,63 +46,63 @@ class cURLTest extends PHPUnit_Framework_TestCase
 		$this->assertNotNull($r->info);
 	}
 
-	/** @test */
+	#[Test]
 	public function queryRequestBody()
 	{
 		$r = $this->makeCurl()->post(static::URL.'/echo.php', array('foo' => 'bar'));
 		$this->assertEquals('foo=bar', $r->body);
 	}
 
-	/** @test */
+	#[Test]
 	public function queryRequestEmptyArrayBody()
 	{
 		$r = $this->makeCurl()->post(static::URL.'/echo.php', array());
 		$this->assertEquals('', $r->body);
 	}
 
-	/** @test */
+	#[Test]
 	public function queryRequestEmptyObjectBody()
 	{
 		$r = $this->makeCurl()->post(static::URL.'/echo.php', new \stdClass());
 		$this->assertEquals('', $r->body);
 	}
 
-	/** @test */
+	#[Test]
 	public function jsonRequestBody()
 	{
 		$r = $this->makeCurl()->jsonPost(static::URL.'/echo.php', array('foo' => 'bar'));
 		$this->assertEquals('{"foo":"bar"}', $r->body);
 	}
 
-	/** @test */
+	#[Test]
 	public function jsonRequestEmptyArrayBody()
 	{
 		$r = $this->makeCurl()->jsonPost(static::URL.'/echo.php', array());
 		$this->assertEquals('[]', $r->body);
 	}
 
-	/** @test */
+	#[Test]
 	public function jsonRequestEmptyObjectBody()
 	{
 		$r = $this->makeCurl()->jsonPost(static::URL.'/echo.php', new \stdClass());
 		$this->assertEquals('{}', $r->body);
 	}
 
-	/** @test */
+	#[Test]
 	public function rawRequestBody()
 	{
 		$r = $this->makeCurl()->rawPost(static::URL.'/echo.php', '<foo/>');
 		$this->assertEquals('<foo/>', $r->body);
 	}
 
-	/** @test */
+	#[Test]
 	public function rawRequestEmptyBody()
 	{
 		$r = $this->makeCurl()->rawPost(static::URL.'/echo.php', '');
 		$this->assertEquals('', $r->body);
 	}
 
-	/** @test */
+	#[Test]
 	public function fileUpload()
 	{
 		$file = __FILE__;
@@ -114,7 +116,7 @@ class cURLTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(basename($file)."\t".filesize($file)."\n", $r->body);
 	}
 
-	/** @test */
+	#[Test]
 	public function digestAuth()
 	{
 		$curl = $this->makeCurl();
@@ -125,28 +127,28 @@ class cURLTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(200, $response->statusCode);
 	}
 
-	/** @test */
+	#[Test]
 	public function throwsExceptionOnCurlError()
 	{
 		$this->setExpectedException('anlutro\cURL\cURLException', 'cURL request failed with error [7]:');
 		$this->makeCurl()->get('http://0.0.0.0');
 	}
 
-	/** @test */
+	#[Test]
 	public function throwsExceptionWithMissingUrl()
 	{
 		$this->setExpectedException('BadMethodCallException', 'Missing argument 1 ($url) for anlutro\cURL\cURL::get');
 		$this->makeCurl()->get();
 	}
 
-	/** @test */
+	#[Test]
 	public function throwsExceptionWhenDataProvidedButNotAllowed()
 	{
 		$this->setExpectedException('InvalidArgumentException', 'HTTP method [options] does not allow POST data.');
 		$this->makeCurl()->options('http://localhost', array('foo' => 'bar'));
 	}
 
-	/** @test */
+	#[Test]
 	public function defaultHeadersAreAdded()
 	{
 		$curl = $this->makeCurl();
@@ -155,7 +157,7 @@ class cURLTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('bar', $request->getHeader('foo'));
 	}
 
-	/** @test */
+	#[Test]
 	public function defaultOptionsAreAdded()
 	{
 		$curl = $this->makeCurl();
@@ -164,7 +166,7 @@ class cURLTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('bar', $request->getOption('foo'));
 	}
 
-	/** @test */
+	#[Test]
 	public function curloptFileWorks()
 	{
 		$r = $this->makeCurl()
