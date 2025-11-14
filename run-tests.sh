33d846de849262af364f1e01b3c1417a6fcda0d9
Andreas Lutro <anlutro@gmail.com>
2025-11-14 12:16:36 +0100

    set env var

--- run-tests.sh
+++ run-tests.sh
@@ -4,4 +4,4 @@ php -S localhost:8080 -t tests/server > /dev/null 2>&1 &
 php_s_pid=$!
 cleanup() { kill $php_s_pid; }
 trap cleanup EXIT
-vendor/bin/phpunit $@
+CURL_TEST_SERVER_RUNNING=1 vendor/bin/phpunit $@
